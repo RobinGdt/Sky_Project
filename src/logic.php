a10746129983dfe4daf651e58c86083ec1b0cc34
Arkansiel92 <yaj92250@hotmail.fr>
2022-10-14 15:11:40 +0200

    added create articles

--- src/logic.php
+++ src/logic.php
@@ -8,21 +8,5 @@
   } catch(Exception $e) {
     die('Erreur : '.$e->getMessage());
   };
-
-  $sql = "SELECT * FROM data";
-
-  if(isset($_REQUEST["new_post"])){
-    $title = $_REQUEST['title'];
-    $content = $_REQUEST['content'];
-
-    $stmt = $pdo->prepare("INSERT INTO data(title,content) VALUES(':title', ':content')");
-    $stmt->execute([
-      'title' => $title,
-      'content' => $content
-    ]);
-    $fetch = $stmt->fetch();
-
-    echo "Post created succefully";
-    header("Location: index.php?info=added");
-  }
+  
 ?>
